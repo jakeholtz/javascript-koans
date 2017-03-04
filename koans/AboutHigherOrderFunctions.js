68302f91f0f29ba53e35ff6ffeb37e83ae1ec5f8
Jake Holtz <jahholtz@gmail.com>
2017-03-04 12:23:15 -0800

    ALL FIXED AboutHigherOrderFunctions.js

--- koans/AboutHigherOrderFunctions.js
+++ koans/AboutHigherOrderFunctions.js
@@ -84,12 +84,12 @@ describe("About Higher Order Functions", function() {
 
   it("should use chain() ... .value() to use multiple higher order functions", function() {
     var result = _([ [0, 1], 2 ]).chain()
-                     .flatten()
-                     .map(function(x) { return x+1 } )
-                     .reduce(function(sum, x) { return sum + x })
-                     .value();
+                     .flatten() // [0, 1, 2]
+                     .map(function(x) { return x+1 } ) // [1, 2, 3]
+                     .reduce(function(sum, x) { return sum + x }) // [6]
+                     .value(); // 6
 
-    expect(result).toEqual(FILL_ME_IN);
+    expect(result).toEqual(6);
   });
 
 });
