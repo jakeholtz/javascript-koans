53dc84edb63c2383c8cf9207a47ca673699edf5e
Jake Holtz <jahholtz@gmail.com>
2017-03-04 16:55:42 -0800

    FIXED "should add all the natural numbers below 1000"

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -56,13 +56,19 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
     
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
-
-    expect(233168).toBe(FILL_ME_IN);
+    var sum = _.range(1, 1000).reduce( function(a, b) {
+      if (b % 3 === 0 || b % 5 === 0) {
+        return a + b;
+      } else {
+        return a;
+      }
+    }, 0);
+     
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
